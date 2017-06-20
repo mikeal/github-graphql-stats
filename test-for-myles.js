3db475955868de9e6f7aa7a8331c6a1e3794c04d
Mikeal Rogers <mikeal.rogers@gmail.com>
2017-06-20 16:37:51 -0700

    Working query for Myles.

--- test-for-myles.js
+++ test-for-myles.js
@@ -5,11 +5,50 @@ const pullRequests = require('./lib/pull-requests')
 if (!process.env.GHTOKEN) throw new Error('Must set GHTOKEN env variable.')
 const token = process.env.GHTOKEN
 
+// ;(async () => {
+//   let states = ['MERGED', 'CLOSED']
+//   let labels = ['dns', 'aix']
+//   let opts = {token, states, labels, baseRefName: 'master'}
+//   let pulls = await pullRequests('nodejs/node', opts)
+//   console.log(pulls)
+// })()
 
-;(async () => {
-  let states = ['MERGED', 'CLOSED']
-  let labels = ['dns', 'aix']
-  let opts = {token, states, labels, baseRefName: 'master'}
-  let pulls = await pullRequests('nodejs/node', opts)
-  console.log(pulls)
-})()
+const apollo = require('apollo-client')
+const tag = require('graphql-tag')
+
+const search = 'repo:nodejs/node is:pr 13823 13828'
+
+module.exports = (name, opts) => {
+  const networkInterface = apollo.createNetworkInterface({
+    uri: 'https://api.github.com/graphql',
+    opts: {
+      headers: {
+        Authorization: `bearer ${token}`,
+        'User-Agent': 'stats-to-spreadsheet-v0.0.1'
+      }
+    }
+  })
+  const client = new apollo.ApolloClient({networkInterface})
+  let query = tag`
+  query {
+    search (query: "${search}", type: ISSUE, first: 10) {
+      nodes {
+        ... on PullRequest {
+          labels(first: 100) {
+            nodes {
+              name
+            }
+          }
+          number
+        }
+      }
+    }
+  }
+  `
+  return (async () => {
+    console.log('q')
+    let r = await client.query({query})
+    console.log(r.data.search)
+  })()
+}
+module.exports()
